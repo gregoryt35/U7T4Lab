b58a044ed7c7bf5ce75704545ede633c4fe5e485
BT_1E10_31 <sample@bths.edu>
2022-02-10 10:37:56 -0500

    updated ArrayListAlgorithms

--- src/ArrayListAlgorithms.java
+++ src/ArrayListAlgorithms.java
@@ -233,7 +233,91 @@ public class ArrayListAlgorithms
      */
     public static ArrayList<String> parseWordsAndReverse(String sentence)
     {
+        ArrayList<String> returnList = new ArrayList<>();
+        String updated = sentence;
 
+        int idx = 0;
+        while (updated.indexOf(" ") != -1) {
+            idx = updated.indexOf(" ");
+            String wrd = updated.substring(0, idx);
+            returnList.add(0, wrd);
+            updated = updated.substring(idx + 1);
+        }
+        returnList.add(0, updated);
+
+        return returnList;
+    }
+
+    /** Removes all words from wordList that begin with "b" and inserts them at the
+     *  front of wordList; all "b" words that are moved should appear in the same order
+     *  in the modified arrayList as they did before being moved
+     *
+     *  For example, this method will take a wordList:
+     *  ["apple", "banana", "cherry", "donut", "bagel", "danish", "berry", "baguette", "soda"]
+     *  and modify it to
+     *  ["banana", "bagel", "berry", "baguette", "apple", "cherry", "donut", "danish", "soda"]
+     *
+     *  DOES mutate (modify) elements in wordList
+     *  PRECONDITIONS: wordList.size() > 0, all strings in wordList have at least one character
+     *
+     *  @param wordList  arraylist of words
+     */
+    public static void moveBWords(ArrayList<String> wordList)
+    {
+        int nextIdx = 0;
+        for (int i = 0; i < wordList.size(); i++) {
+            if (wordList.get(i).substring(0, 1).equals("b")) {
+                String removed = wordList.remove(i);
+                wordList.add(nextIdx, removed);
+                nextIdx ++;
+            }
+        }
+    }
+
+    /** Returns arraylist of Integers that contain all mode(s) of the int array numList.
+     *  If elements in numList all appear exactly once, there is no mode, and this method
+     *  should return an empty list
+     *
+     *  For example, if numList is: [1, 2, 3, 2, 4, 5, 5, 6],
+     *  then numList contains two modes: 2, 5
+     *  and this method returns an arraylist containing 2 and 5 (in any order)
+     *  If numList is: [1, 2, 3, 2, 4, 5, 5, 6, 6, 7, 6],
+     *  then numList contains one mode: 6
+     *  and this method returns an arrayList containing 6
+     *  If numList is: [1, 2, 3, 4, 5, 6], then numList contains no mode
+     *  and this method returns an empty arrayList: []
+     *  If numList is: [2, 2, 2, 3, 3, 3, 4, 4, 4],
+     *  then numList contains three modes: 2, 3, 4
+     *  and this method returns an arrayList containing 2, 3, and 4 (in any order)
+     *
+     *  Does NOT mutate (modify) elements in numList
+     *  PRECONDITIONS: numList.length > 0
+     *
+     *  @param numList  numList of ints
+     */
+    public static ArrayList<Integer> modes(int[] numList)
+    {
+        ArrayList<Integer> returnList = new ArrayList<>();
+
+        int max = 1;
+        for (int i = 0; i < numList.length; i++) {
+            int count = 1;
+
+            for (int j = i + 1; j < numList.length; j++) {
+                if (numList[i] == numList[j]) {
+                    count ++;
+                }
+            }
+            if (count > max) {
+                max = count;
+                returnList.clear();
+                returnList.add(numList[i]);
+            }
+            else if (count == max && count != 1) {
+                returnList.add(numList[i]);
+            }
+        }
+        return returnList;
     }
 
 }
